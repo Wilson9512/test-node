4977c8c9dcc61e094df91df288cc5c9e71409316
Wilson#122 <cwhth2956@gmail.com>
2022-01-15 19:16:30 +0800

    require/module.exports 2

--- practice/arrow-func.js
+++ practice/arrow-func.js
@@ -1,5 +1,11 @@
 
 const f1 = a=>a*a;
+const f3 = a=> [...Array(b - a   1)].map((e, i) => a   i)
+Array(b - a   1).fill(' ').map((e, i) => a   i)
+[...Array(b - a   1).entries()].map(e => e[0]   a)
+[...Array(b - a   1).keys()].map(e => e   a);
+
+
 console.log(f1(7));
 console.log('1:',__dirname);
 
--- practice/require01.js
+++ practice/require01.js
@@ -1,8 +1,8 @@
 
 const f1 = require('./arrow-func');
-const f2 = require('./arrow-func');
+const f2 = require(__dirname + '/arrow-func');
 console.log('2:',__dirname);
 
 
 console.log(f1(9));
-console.log(f1(10));
+console.log(f2(10));
