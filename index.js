1a178c84e01daa36448b6144d57165860c2aa379
Wilson#122 <cwhth2956@gmail.com>
2022-01-16 20:55:53 +0800

    404 頁面

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const express = require('express');
 
 const app = express();
 
+
+// *** 路由定義開始 :BEGIN
 app.get('/',(req,res)=>{
 res.send(`<h2>Hello</h2>`)
 });//路徑跟方法
+
+
+// *** 路由定義結束:END
+
+app.use((req,res)=>{
+    res.status(404).send(`<h1>找不到這頁</h1>`)
+})
+
+
+
 let port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log(`NODE_ENV:${process.env.NODE_ENV}`);
