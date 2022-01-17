a9236eb1460a688b23ce9d2731e5fa46445f851a
Wilson#122 <cwhth2956@gmail.com>
2022-01-17 21:45:13 +0800

    Top-level middleware

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+//拿到中介函式/軟體//放在此處變Top-level middleware 像過濾器或是閘門
+app.use(express.urlencoded({extended: false}));//use是所有的方法都會進來
+app.use(express.json());
 app.use(express.static('public'));
+
 app.use('/jquery', express.static('node_modules/jquery/dist'));
 app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
 
@@ -28,19 +32,20 @@ app.get('/json-sales',(req,res) =>{
 res.render('json-sales',{sales});//將路由名稱取為樣板名
 });
 
-//拿到中介函式/軟體
-const urluncodedParser = express.urlencoded({extended: false});
-const jsonParser = express.json();
-//用在post 當第二個參數 第三個才是處理器/回呼函式
-app.post('/try-post',[urluncodedParser,jsonParser],(req,res) =>{
-    res.json(req.body);
-});
 app.get('/try-qs',(req,res) =>{
     res.json(req.query);
 });
 
+
+//用在post 當第二個參數 第三個才是處理器/回呼函式
+//express會判斷Content-Type現在需要哪個middleware處理
+app.post('/try-post',(req,res) =>{
+    res.json(req.body);
+});
+
 // *** 路由定義結束:END
 
+//設定404頁面
 app.use((req, res) => {
     res.status(404).send(`<h1>找不到這頁</h1>`)
 })
