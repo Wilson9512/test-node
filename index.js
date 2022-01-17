94618096a74076a91906eb1cc4a95b59c0c9de69
Wilson#122 <cwhth2956@gmail.com>
2022-01-17 19:06:23 +0800

    ejs 樣板設定

--- index.js
+++ index.js
@@ -4,26 +4,27 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use('/jquery',express.static('node_modules/jquery/dist'));
-app.use('/bootstrap',express.static('node_modules/bootstrap/dist'));
+app.use('/jquery', express.static('node_modules/jquery/dist'));
+app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
 
 // *** 路由定義開始 :BEGIN
-app.get('/',(req,res)=>{
-res.send(`<h2>Hello</h2>`)
+app.get('/', (req, res) => {
+    //res.send(`<h2>Hello</h2>`)
+    res.render('home',{name:'Wil'})
 });//路徑跟方法
 
 
 // *** 路由定義結束:END
 
-app.use((req,res)=>{
+app.use((req, res) => {
     res.status(404).send(`<h1>找不到這頁</h1>`)
 })
 
 
-
 let port = process.env.PORT || 3000;
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`NODE_ENV:${process.env.NODE_ENV}`);
     console.log(`啟動:${port}`, new Date());
 });
