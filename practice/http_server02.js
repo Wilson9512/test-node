7b3924e4d23fd3f767760bded13782267baff9d3
Wilson#122 <cwhth2956@gmail.com>
2022-01-25 14:55:00 +0800

    content type 加入編碼

--- practice/http_server02.js
+++ practice/http_server02.js
@@ -2,16 +2,18 @@ const http = require('http');
 const fs = require('fs');
 const server = http.createServer((req, res) => {
     res.writeHead(200, {
-        'Content-Type': 'text/html'
+        'Content-Type': 'text/html; charset=utf-8'
     })
     fs.writeFile(
-        'heagv0der.txt',
+        './header.txt',
         JSON.stringify(req.headers, null, 4),
         err => {
             if (err) {
-                res.end(`<h1>錯誤 :${error}</h1>`);
+                // res.end(`<h1>錯誤 :${error}</h1>`);
+                res.end('錯誤');
             }else {
-                res.end(`<h2>ok</h2>`);
+                // res.end(`<h2>ok</h2>`);
+                res.end('沒問題');
             }
         });
 
