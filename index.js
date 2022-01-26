626e89f0b8d9c4ff2473ba22593cd90368a7d655
Wilson#122 <cwhth2956@gmail.com>
2022-01-26 14:02:32 +0800

    新增註解

--- index.js
+++ index.js
@@ -4,34 +4,32 @@ const express = require('express');
 const multer = require('multer');
 const fs = require('fs').promises;
 const session = require('express-session');
-const MysqlStore = require('express-mysql-session')(session);//require('express-mysql-session')是一個function
+const MysqlStore = require('express-mysql-session')(session); //require('express-mysql-session')是一個function
 const moment = require('moment-timezone');
 const upload = multer({dest: 'tmp_uploads/'});
 const uploadImg = require('./modules/upload-images');
 const db = require('./modules/connect-mysql');
-const sessionStore = new MysqlStore({}, db);//object本來要設定連線資料庫的設定,現在可以透過套件直接連線到資料庫了
+const sessionStore = new MysqlStore({}, db); //object本來要設定連線資料庫的設定,現在可以透過套件直接連線到資料庫了
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');//設定(set)樣版引擎,使用views資料夾名不用另外設定
 
 //拿到中介函式/軟體//放在此處變Top-level middleware 像過濾器或是閘門//use是不管什麼方法都接收
 app.use(session({
     name:'mySessionId',
-    //初始化設定:新用戶沒有用到session時要不要建立session跟發送cookie
-    saveUninitialized: false,
-    //沒有變更要不要強制回存
-    resave: false,
-    secret:'fd99wet3zws',
+    saveUninitialized: false, //初始化設定:新用戶沒有用到session時要不要建立session跟發送cookie
     store: sessionStore,
+    resave: false,//沒有變更要不要強制回存
+    secret:'fd99wet3zws',
     cookie:{
         //設定用戶cookie什麼時候過期
-        maxAge: 1200000, //20分鐘--ms
+        maxAge: 1800000, //30分鐘--ms
     },
 }));
 app.use(express.urlencoded({extended: false}));//use是所有的方法都會進來
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('public'));//靜態內容資料夾設定
 
 app.use('/jquery', express.static('node_modules/jquery/dist'));
 app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
@@ -45,7 +43,6 @@ app.use((req,res,next)=>{
 
 app.get('/', (req, res) => {
     res.locals.title = '首頁 -' + res.locals.title;
-
     //res.send(`<h2>Hello</h2>`)
     res.render('home', {name: 'Wil'});
 });//路徑跟方法
@@ -60,7 +57,6 @@ app.get('/json-sales', (req, res) => {
 app.get('/try-qs', (req, res) => {
     res.json(req.query);
 });
-
 //中介函式用在post 當第二個參數 第三個才是處理器/回呼函式
 //express會判斷Content-Type現在需要哪個middleware處理
 app.post('/try-post', (req, res) => {
@@ -120,7 +116,8 @@ app.get(/^\/m\/09\d{2}-?\d{3}-?\d{3}$/i, (req, res) => {
 });
 
 app.use(require('./routes/admin2'));//路由模組：當中介器require來用
-app.use('/admin3',require('./routes/admin3'));//路由模組：當中介器require來用
+app.use('/admin3', require('./routes/admin3'));//路由模組：當中介器require來用
+app.use('/member', require('./routes/member'));
 
 app.get('/try-sess',(req,res)=>{
     //自定一個參數名:不能叫cookie
@@ -147,6 +144,7 @@ app.get('/try-db', async (req,res)=>{
         ]);
     res.json(r);
 });
+
 // *** 路由定義結束:END
 
 //設定404頁面
@@ -159,5 +157,5 @@ let port = process.env.PORT || 3000;
 const node_env = process.env.NODE_ENV || 'development';
 app.listen(port, () => {
     console.log(`NODE_ENV:${node_env}`);
-    console.log(`啟動: ${port}`, new Date());
+    console.log(`啟動: ${port} - `, new Date());
 });
