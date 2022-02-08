5f28cd0e151f912d7912b39120cc20e8d634a80f
Wilson#122 <cwhth2956@gmail.com>
2022-02-08 15:01:27 +0800

    設定好routes 可以手動切換頁數

--- routes/member.js
+++ routes/member.js
@@ -25,6 +25,11 @@ router.get('/list', async (req, res) => {
     if(totalRows){
         output.totalPages = Math.ceil(totalRows/perPage);
         output.totalRows = totalRows;
+
+        const sql = `SELECT * FROM member LIMIT ${perPage*
+        (page-1)},${perPage}`;
+        const [rs2] = await db.query(sql);
+        output.rows = rs2;
     }
 
     res.json(output);
